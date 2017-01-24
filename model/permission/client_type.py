9cda68cf870ed53be521a2f96b40efdecda6db29
Robert <teslar.account@web.de>
2017-01-24 14:40:41 +0100

    description zu ClientType und CredentialType hinzugefügt

--- model/permission/client_type.py
+++ model/permission/client_type.py
@@ -2,4 +2,4 @@ from run import db
 class ClientType(db.Model):
     client_type_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
-#keine description?
+    description = db.Column(db.String)
--- model/permission/credential_type.py
+++ model/permission/credential_type.py
@@ -2,4 +2,4 @@ from run import db
 class CredentialType(db.Model):
     credential_type_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
-    #keine description?
+    description = db.Column(db.String)
