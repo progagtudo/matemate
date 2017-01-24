f8ea558e9648922f37f4cfb169b434527c139b25
Robert <teslar.account@web.de>
2017-01-24 13:19:54 +0100

    CredentialType hinzugefügt

---
+++ model/permission/credential_type.py
@@ -0,0 +1,5 @@
+from run import db
+class CredentialType(db.Model):
+    credential_type_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    #keine description?
