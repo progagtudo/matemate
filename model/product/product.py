15c8ebde8d818b84e78e38576fcca2394079e5db
Robert <teslar.account@web.de>
2017-01-24 15:37:21 +0100

    removed unreasonable nullable = false

--- model/product/product.py
+++ model/product/product.py
@@ -6,9 +6,9 @@ from run import db
 class Product(db.Model):
     product_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
     price = db.Column(db.Numeric, nullable=False)
     is_sale_prohibited = db.Column(db.Boolean, nullable=False)
     is_default_redemption = db.Column(db.Boolean, nullable=False)
     tax_category_id = db.Column(db.Integer, ForeignKey("tax_category.tax_category_id"), nullable=False)
-    product_category_id = db.Column(db.Integer, ForeignKey("product_category.product_category_id"), nullable=True)
+    product_category_id = db.Column(db.Integer, ForeignKey("product_category.product_category_id"))
--- model/product/product_category.py
+++ model/product/product_category.py
@@ -5,5 +5,5 @@ from run import db
 class ProductCategory(db.Model):
     product_category_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    category_icon_uri = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    category_icon_uri = db.Column(db.String)
