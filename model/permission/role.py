5b3c772352d42a5117d8983cfe15bace3cea09d2
Robert <teslar.account@web.de>
2017-01-24 13:07:12 +0100

    Benennungsanpassung bei Role, Right und ProductTag

--- model/permission/role.py
+++ model/permission/role.py
@@ -1,6 +1,6 @@
 from run import db
-class AvailableRight(db.Model):
-    right_id = db.Column(db.Integer, primary_key=True)
+class Role(db.Model):
+    role_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     description = db.Column(db.String)
 
--- model/product/product_tag.py
+++ model/product/product_tag.py
@@ -1,5 +1,5 @@
 from run import db
-class AvailableProductTag(db.Model):
+class ProductTag(db.Model):
     tag_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     description = db.Column(db.String)
