88181e37c0ccc6cda07c004df7de14918d667903
Robert <teslar.account@web.de>
2017-01-24 17:31:28 +0100

    foreign key foo
    added foreign key for customer and staff
    removed foreign key in product, see TODO

--- model/person/customer.py
+++ model/person/customer.py
@@ -1,11 +1,13 @@
 import datetime
 
+from sqlalchemy import ForeignKey
+
 from run import db
 from model.person.person import Person
 
 
 class Customer(db.Model):
-    customer_id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, ForeignKey("person.person_id"), primary_key=True)
     base_balance = db.Column(db.Numeric, default=0, nullable=False)
     base_balance_date = db.Column(db.DateTime, default=datetime.datetime.now, nullable=False)
     added_date = db.Column(db.DateTime, default=datetime.datetime.now, nullable=False)
