71848191a3775a14e49015ff960360c6b42c46fa
Robert <teslar.account@web.de>
2017-06-02 15:24:40 +0200

    added 2 primary keys

--- model/money/redeemer_for.py
+++ model/money/redeemer_for.py
@@ -2,7 +2,8 @@ from sqlalchemy import ForeignKey
 from run import db
 
 
+# TODO Primary key correct?
 class RedeemerFor(db.Model):
-    staff_id = db.Column(db.Integer, ForeignKey("staff.staff_id"), nullable=False)
-    repayment_id = db.Column(db.Integer, ForeignKey("repayment.repayment_id"), nullable=False)
+    staff_id = db.Column(db.Integer, ForeignKey("staff.staff_id"), nullable=False, primary_key=True)
+    repayment_id = db.Column(db.Integer, ForeignKey("repayment.repayment_id"), nullable=False, primary_key=True)
     acknowledged_date = db.Column(db.DateTime, nullable=False)
